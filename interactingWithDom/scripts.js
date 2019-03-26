2d761b9889829ae45dcef0c35028d5f3cc9a9192
April Copes <antdevelopment1@gmail.com>
2019-03-26 19:38:23 -0400

    renders html element as a property of an object to the screen

--- interactingWithDom/scripts.js
+++ interactingWithDom/scripts.js
@@ -1,8 +1,9 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Hellow World!',
-        link: 'http://google.com'
+        title: 'Hello World!',
+        link: 'http://google.com',
+        finishLink: '<a href="http://google.com">Google</a>'
     },
     methods: {
         sayHello() {
@@ -11,7 +12,8 @@ new Vue({
         getTitle() {
             // Even though we are inside this methods object and traditionally the this keyword would point to this object vue does some behind the scenes magic for us to re can redirect the keyword of this to point to our data object
             // Thanks vue
+            this.title = 'I am a title that has been updated'
             return this.title;
-        }
+        },
     }
 });
