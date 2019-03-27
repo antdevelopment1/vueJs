4d57b3aa08e79de2797547d05b5eeae97760a253
April Copes <antdevelopment1@gmail.com>
2019-03-26 20:29:31 -0400

    adds update coorinates on mousemove on paragraph

--- interactingWithDom/scripts.js
+++ interactingWithDom/scripts.js
@@ -1,12 +1,18 @@
 new Vue({
     el: '#app',
     data: {
-        counter: 0
+        counter: 0,
+        x: 0,
+        y: 0
     },
     methods: {
         increase() {
             this.counter = this.counter + 1;
             // this.counter++;
+        },
+        updateCoordinates() {
+            this.x = event.clientX;
+            this.y = event.clientY;
         }
     }
 });
