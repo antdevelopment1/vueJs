acfb238776d22b28e5bf2d16cf339e36fdabe37c
April Copes <antdevelopment1@gmail.com>
2019-03-27 19:56:38 -0400

    adds watch computed function for asynchromous tasks

--- interactingWithDom/scripts.js
+++ interactingWithDom/scripts.js
@@ -10,6 +10,14 @@ new Vue({
             return this.counter > 5 ? 'Greater than 5' : 'Less than 5';
         }
     },
+    watch: {
+        counter(value) {
+            var vm = this;
+            setTimeout(function() {
+                vm.counter = 0;
+            }, 2000)
+        }   
+    },
     methods: {
         result() {
             console.log('Method')
