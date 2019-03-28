9ed72880ca16f0e38e1934d8e144d55b7af7d533
April Copes <antdevelopment1@gmail.com>
2019-03-27 22:37:10 -0400

    Adds commands to alter colors in css to create clicking eefect

--- interactingWithDom/scripts.js
+++ interactingWithDom/scripts.js
@@ -1,19 +1,8 @@
 new Vue({
     el: "#app",
     data: {
-        value: 0
+        attachRed: false,
+        attachGreen: false,
+        attachBlue: false
     },
-    computed: {
-        result() {
-            return this.value >= 37 ? 'Done' : 'Not there yet'
-        }
-    },
-    watch: {
-        result() {
-            let vm = this;
-            setTimeout(function() {
-                vm.value = 0;
-            }, 2000)
-        }
-    }
 })
