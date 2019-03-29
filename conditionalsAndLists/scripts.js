74dcfddd92c6618f5f6fbdf12278d6d257c3df3c
April Copes <antdevelopment1@gmail.com>
2019-03-28 22:06:37 -0400

    part 1 of excercises 1

--- conditionalsAndLists/scripts.js
+++ conditionalsAndLists/scripts.js
@@ -1,10 +1,6 @@
 new Vue({
     el: '#app',
     data: {
-        ingrediants: ["meal", "fruit", "cookies"],
-        persons: [
-            {name: 'Max', age: 27, color: 'red'},
-            {name: 'Anna', age: 'unknown', color: 'blue'}
-        ]
+        show: true
     }
 })
