abba2149ed2791ecd18b77738fb0b04c365dd6df
April Copes <antdevelopment1@gmail.com>
2019-03-28 20:28:17 -0400

    Adds example for if statement for grouping showing and hiding unnested elements

--- interactingWithDom/scripts.js
+++ interactingWithDom/scripts.js
@@ -3,13 +3,20 @@ new Vue({
     data: {
         attachRed: false,
         attachGreen: false,
-        attachBlue: false
+        attachBlue: false,
+        color: 'green',
+        width: 100
     },
     computed: {
         divClasses() {
             return {
                 red: this.attachRed,
                 blue: !this.attachRed
+            }
+        },
+        myStyle() {
+            return { backgroundColor: this.color,
+                width: this.width + 'px'
             }
         }
     }
