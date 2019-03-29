08b1dc8bff460c1bfd87e669f286b2e27ecbead9
April Copes <antdevelopment1@gmail.com>
2019-03-28 22:23:30 -0400

    adds fourth part to excersies. Vue is f'n cool

--- conditionalsAndLists/scripts.js
+++ conditionalsAndLists/scripts.js
@@ -3,6 +3,7 @@ new Vue({
     data: {
         show: true,
         numbers: [1,2,3,4,5],
-        stuff: {title: 'Lord of the Rings', author: 'J.R.R. Tolkiens', books: '3'}
+        stuff: {title: 'Lord of the Rings', author: 'J.R.R. Tolkiens', books: '3'},
+        nextStuff: {name: 'TESTOBJECT', data: [1.67, 1.33, 0.98, 2.21]}
     }
 })
