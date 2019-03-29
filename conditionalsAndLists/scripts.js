07eb9499ab72a4997e5a105bfa75fdcf8bffddb4
April Copes <antdevelopment1@gmail.com>
2019-03-28 22:14:35 -0400

    third part of excersies

--- conditionalsAndLists/scripts.js
+++ conditionalsAndLists/scripts.js
@@ -2,6 +2,7 @@ new Vue({
     el: '#app',
     data: {
         show: true,
-        numbers: [1,2,3,4,5]
+        numbers: [1,2,3,4,5],
+        stuff: {title: 'Lord of the Rings', author: 'J.R.R. Tolkiens', books: '3'}
     }
 })
