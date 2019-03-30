97fba7a4dfdf288424294af9a6f3752e87c68ab9
April Copes <antdevelopment1@gmail.com>
2019-03-30 14:28:22 -0400

    hooks up give up function

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -34,7 +34,7 @@ new Vue({
             this.monsterAttacks();
         },
         giveUp() {
-
+            this.gameIsRunning = false;
         },
         monsterAttacks() {
             this.playerHealth -= this.calculateDamage(5, 12);
