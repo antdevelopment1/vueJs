50a0e653a3f075c356296d475427d118ade8c171
April Copes <antdevelopment1@gmail.com>
2019-03-30 11:54:15 -0400

    Adds functionality to attack player and monster

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -10,6 +10,35 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack() {
+            let max = 10;
+            let min = 3;
+            let damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.monsterHealth -= damage;
+            if (this.monsterHealth <= 0) {
+                alert('We won.');
+                this.gameIsRunning = false;
+                return;
+            }
+            max = 12;
+            min = 5;
+            damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.playerHealth -= damage;
+            if (this.playerHealth <= 0) {
+                alert('You lost');
+                this.gameIsRunning = false;
+                // We don't need a return because this will the last line of code in this function
+            }
+        },
+        specialAttack() {
+
+        },
+        heal() {
+
+        },
+        giveUp() {
+
         }
     }
 })
