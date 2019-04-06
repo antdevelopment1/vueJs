a4afc5bb5e346a59eb2d04f681babcd74fb5a148
April Copes <antdevelopment1@gmail.com>
2019-04-05 22:36:00 -0400

    begining to play with components

--- vueWebpackCli/vue-cli/src/main.js
+++ vueWebpackCli/vue-cli/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+Vue.component('my-cmp', {
+  data: function () {
+    return {
+      status: "Critical",
+    }
+  },
+  template: '<p>Server Status: {{ status }}</p>'
+});
+
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  // render: h => h(App)
 })
