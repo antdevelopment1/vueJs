5788d3e7f4e3c361be07eb2447f0e956a7d5eef3
April Copes <antdevelopment1@gmail.com>
2019-04-13 21:36:21 -0600

    Adds file set up

---
+++ vueWebpackCli/start/src/components/Server.vue
@@ -0,0 +1,14 @@
+<template>
+    <li class="list-group-item">
+        Server #{{ id }}
+    </li>
+</template>
+
+<script>
+    export default {
+        props: ['id']
+    }
+</script>
+
+<style>
+</style>
