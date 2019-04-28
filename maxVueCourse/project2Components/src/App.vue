47d084d8889e436a08922ee34bf58b878211bfa3
April Copes <antdevelopment1@gmail.com>
2019-04-28 13:52:22 -0600

    Another example of using dynamic components or compoment components

---
+++ maxVueCourse/project2Components/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="selectedComponent = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedComponent = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedComponent = 'appRed'">Load Red Template</button>
+                <hr>
+                <!-- <app-blue>This is the content</app-blue>
+                <app-green>This is the content</app-green>
+                <app-red>This is the content</app-red> -->
+                <component :is="selectedComponent">Default Component</component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data() {
+            return {
+                selectedComponent: 'appBlue'
+            }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
