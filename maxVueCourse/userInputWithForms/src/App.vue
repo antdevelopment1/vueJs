06a5b9d5e5aa7ce514d1ed8941b9522ab477615c
April Copes <antdevelopment1@gmail.com>
2019-04-28 18:18:19 -0600

    Adds example for v-model on checkboxes

--- maxVueCourse/userInputWithForms/src/App.vue
+++ maxVueCourse/userInputWithForms/src/App.vue
@@ -51,13 +51,17 @@
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail"
+                                    v-model="sendMail"
+                            > Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail"
+                                    v-model="sendMail"
+                            > Send Infomail
                         </label>
                     </div>
 
@@ -112,7 +116,7 @@
                         <p style="white-space: pre">Message: {{ message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="(item, index) in sendMail" :key="index">{{item}}</li>
                         </ul>
                         <p>Gender:</p>
                         <p>Priority:</p>
@@ -133,7 +137,8 @@
                     password: '',
                     age: 27
                 },
-                message: 'A new Text'
+                message: 'A new Text',
+                sendMail: []
             }
         }
     }
